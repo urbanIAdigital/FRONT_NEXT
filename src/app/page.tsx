07bd3fb572a9331cc2c3f0e542b9ae18f27ef8c1
Emerson David Fernandez Regino <emerson.fernandez@edu.gov.co>
2025-05-02 15:24:04 -0500

    Botom pruebas sombras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-    <button className="bg-primary p-3">CLICK</button>
+    <button className="bg-blue-500 border border-blue-300 shadow-md p-3">CLICK</button>
   )
 }
